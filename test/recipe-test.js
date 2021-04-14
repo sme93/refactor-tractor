e59237fe4594503793100ba1be9e2d472ad986aa
mcraig2342 <matthewcookecraig10@gmail.com>
2021-04-14 08:09:31 -0600

    Add additional tests for properties

--- test/recipe-test.js
+++ test/recipe-test.js
@@ -21,6 +21,23 @@ describe('Recipe', () => {
     it('Should hold its own instruction data', () => {
       expect(recipe.instructions).to.equal(recipeData[47].instructions);
     })
+
+    it('Should store a name', () => {
+      expect(recipe.name).to.equal(recipeData[47].name);
+    })
+
+    it('Should store an id', () => {
+      expect(recipe.id).to.equal(recipeData[47].id);
+    })
+
+    it('Should store tags', () => {
+      expect(recipe.tags).to.equal(recipeData[47].tags);
+    })
+
+    it('Should store ingredient data', () => {
+      expect(recipe.ingredientsData).to.deep.equal(ingredientsData);
+    })
+
   })
 
   it('Should be able to calculate the cost of its ingredients', () => {
