db89d36d25e6bf7747f59ca6791e6d1f0eb74f3b
Nichele Dunn <the7yearitch@hotmail.com>
2021-04-13 18:41:51 -0600

    📝 Pseudocode

---
+++ test/pantry-test.js
@@ -0,0 +1,26 @@
+//
+// Pantry Data
+// Create classes and methods that can:
+//
+// Determine whether a user’s pantry has enough ingredients to cook a given meal.
+// Determine the amount of missing ingredients still needed to cook a given meal, based on what’s in the user’s pantry.
+// Remove the ingredients used to cook a given recipe from a user’s pantry.
+// User Stories
+// As a user, I should be able to view what ingredients exist inside of my pantry.
+// As a user, I should be able to check my list of recipes to cook and see if my pantry has enough ingredients to cook a meal.
+// As a user, I should be told what ingredients are still needed if I don’t have enough ingredients in my pantry to cook the recipe.
+// As a user, I should be able to cook a meal if I do have enough ingredients in my pantry. As a result, cooking the meal should remove the ingredients from my pantry.
+
+
+// Testing
+// You should NOT use the original data files in the data directory for testing. These are big files to begin with, and a real-world dataset would have millions of records. That’s far too big to use every time you want to run a test.
+//
+// Instead, for your tests, you should create small, sample datasets that match the structure of the application data. By creating this sample dataset, you will also know if your methods are working correctly because you can do the calculations by hand with a much smaller dataset.
+//
+// You are expected to test:
+// All class properties
+// All class methods and updates to their properties
+// Any helper methods you build out
+
+// You are not expected to test:
+// DOM manipulation / DOM manipulating methods (like document.querySelector(...))
