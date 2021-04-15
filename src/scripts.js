9da4dc750be417be582c23899275b4a5ab3cd97c
mcraig2342 <matthewcookecraig10@gmail.com>
2021-04-15 16:19:01 -0600

    Fix all functions in scripts so that they now work with API data

--- src/scripts.js
+++ src/scripts.js
@@ -101,48 +101,56 @@ function favoriteCard(event) {
 }
 
 function cardButtonConditionals(event) {
-  if (event.target.classList.contains('favorite')) {
-    favoriteCard(event);
-  } else if (event.target.classList.contains('card-picture')) {
-    displayDirections(event);
-  } else if (event.target.classList.contains('home')) {
-    favButton.innerHTML = 'View Favorites';
-    populateCards(cookbook.recipes);
-  }
+  getData()
+    .then(allData => {
+      if (event.target.classList.contains('favorite')) {
+        favoriteCard(event);
+      } else if (event.target.classList.contains('card-picture')) {
+        displayDirections(event);
+      } else if (event.target.classList.contains('home')) {
+        favButton.innerHTML = 'View Favorites';
+        let cookbook = new Cookbook(allData.recipeData);
+        populateCards(cookbook.recipes);
+      }
+    })
 }
 
 
 function displayDirections(event) {
-  let newRecipeInfo = cookbook.recipes.find(recipe => {
-    if (recipe.id === Number(event.target.id)) {
-      return recipe;
-    }
-  })
-  let recipeObject = new Recipe(newRecipeInfo, ingredientsData);
-  let cost = recipeObject.calculateCost()
-  let costInDollars = (cost / 100).toFixed(2)
-  cardArea.classList.add('all');
-  cardArea.innerHTML = `<h3>${recipeObject.name}</h3>
-  <p class='all-recipe-info'>
-  <strong>It will cost: </strong><span class='cost recipe-info'>
-  $${costInDollars}</span><br><br>
-  <strong>You will need: </strong><span class='ingredients recipe-info'></span>
-  <strong>Instructions: </strong><ol><span class='instructions recipe-info'>
-  </span></ol>
-  </p>`;
-  let ingredientsSpan = document.querySelector('.ingredients');
-  let instructionsSpan = document.querySelector('.instructions');
-  recipeObject.ingredients.forEach(ingredient => {
-    ingredientsSpan.insertAdjacentHTML('afterbegin', `<ul><li>
-    ${ingredient.quantity.amount.toFixed(2)} ${ingredient.quantity.unit}
-    ${ingredient.name}</li></ul>
-    `)
-  })
-  recipeObject.instructions.forEach(instruction => {
-    instructionsSpan.insertAdjacentHTML('beforebegin', `<li>
-    ${instruction.instruction}</li>
-    `)
-  })
+  getData()
+    .then(allData => {
+      let cookbook = new Cookbook(allData.recipeData);
+      let newRecipeInfo = cookbook.recipes.find(recipe => {
+        if (recipe.id === Number(event.target.id)) {
+          return recipe;
+        }
+      })
+      let recipeObject = new Recipe(newRecipeInfo, allData.ingredientData);
+      let cost = recipeObject.calculateCost()
+      let costInDollars = (cost / 100).toFixed(2)
+      cardArea.classList.add('all');
+      cardArea.innerHTML = `<h3>${recipeObject.name}</h3>
+      <p class='all-recipe-info'>
+      <strong>It will cost: </strong><span class='cost recipe-info'>
+      $${costInDollars}</span><br><br>
+      <strong>You will need: </strong><span class='ingredients recipe-info'></span>
+      <strong>Instructions: </strong><ol><span class='instructions recipe-info'>
+      </span></ol>
+      </p>`;
+      let ingredientsSpan = document.querySelector('.ingredients');
+      let instructionsSpan = document.querySelector('.instructions');
+      recipeObject.ingredients.forEach(ingredient => {
+        ingredientsSpan.insertAdjacentHTML('afterbegin', `<ul><li>
+        ${ingredient.quantity.amount.toFixed(2)} ${ingredient.quantity.unit}
+        ${ingredient.name}</li></ul>
+        `)
+      })
+      recipeObject.instructions.forEach(instruction => {
+        instructionsSpan.insertAdjacentHTML('beforebegin', `<li>
+        ${instruction.instruction}</li>
+        `)
+      })
+    })
 }
 
 function getFavorites() {
